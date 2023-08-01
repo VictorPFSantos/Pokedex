cb56e5c761a4223a960fc183fed4082bda85784e
Victor P F Santos <victorpfsantos@gmail.com>
2023-08-01 19:19:32 -0300

    Update of pokemon's infos in the right display and functionality of buttons up and down to scroll the informations

--- js/classes/Pokedex.js
+++ js/classes/Pokedex.js
@@ -2,6 +2,7 @@ import { getAllPokemons, getPokemonDetails, getSpeciesDetails } from "../modules
 import { Pokemon } from "./Pokemon.js";
 
 export class Pokedex {
+    // COMPONENTS
     #redLed;
     #yellowLed;
     #greenLed;
@@ -9,20 +10,30 @@ export class Pokedex {
     #leftDisplay;
     #btnPower;
     #btnEnter;
-    #btnUp;
-    #btnRight;
-    #btnDown;
-    #btnLeft;
+    #btnDirectionalUp;
+    #btnDirectionalRight;
+    #btnDirectionalDown;
+    #btnDirectionalLeft;
     #rightDisplay;
-    #pokemons; // OBJECT WITH ALL THE POKEMONS
-    #divPokemon;
-    #pathImages;
-    #currentPokemonIndex = 0;
-    #currentHabitat;
-    #pokemonSound;
-    habitats;
+    #rightDisplayInfos;
     #pokedexNode;
     #pokedexCoverNode;
+    #divPokemon;
+    #pokemonSound;
+    #btnDisplayDirUp;
+    #btnDisplayDirDown;
+
+    // INFOS
+    #pathImages;
+    #pokemons; // OBJECT WITH ALL THE POKEMONS
+    #currentPokemonIndex = 0;
+    #currentHabitat;
+    habitats;
+    #pokedexCoverTransitionTime;
+    #pokedexLeftDisplayTransitionTime;
+    #allCurrentPokemonInfos;
+    #speedScrollRightDisplay = 20;
+    #scrollRightDisplay = 0;
 
     constructor(infos) {
         this.#pathImages = infos.pathImages || 'img/habitats';
@@ -33,13 +44,16 @@ export class Pokedex {
         this.#leftDisplay = infos.leftDisplay || null;
         this.#btnPower = infos.btnPower || null;
         this.#btnEnter = infos.btnEnter || null;
-        this.#btnUp = infos.btnUp || null;
-        this.#btnRight = infos.btnRight || null;
-        this.#btnDown = infos.btnDown || null;
-        this.#btnLeft = infos.btnLeft || null;
+        this.#btnDirectionalUp = infos.btnDirectionalUp || null;
+        this.#btnDirectionalRight = infos.btnDirectionalRight || null;
+        this.#btnDirectionalDown = infos.btnDirectionalDown || null;
+        this.#btnDirectionalLeft = infos.btnDirectionalLeft || null;
         this.#rightDisplay = infos.rightDisplay || null;
+        this.#rightDisplayInfos = infos.rightDisplay.children[1].children[0];
         this.#pokemonSound = infos.pokemonSound || null;
         this.#divPokemon = infos.divPokemon || null;
+        this.#btnDisplayDirUp = infos.btnDisplayDirUp || null;
+        this.#btnDisplayDirDown = infos.btnDisplayDirDown || null;
         this.#pokedexNode = infos.pokedexNode || null;
         this.#pokedexCoverNode = Object.values(infos.pokedexNode.children).find(elem => elem.id === 'part2');
 
@@ -57,18 +71,31 @@ export class Pokedex {
         ];
     }
 
+    get scrollRightDisplay() {
+        return this.#scrollRightDisplay;
+    }
+
+    set scrollRightDisplay(position) {
+        this.#scrollRightDisplay = position;
+    }
+
     // METHODS
     init = async () => {
+        this.#pokedexCoverTransitionTime = parseFloat(getComputedStyle(this.#pokedexCoverNode).getPropertyValue('transition').split(' ')[1].replace('s', '')) * 1000;
+        this.#pokedexLeftDisplayTransitionTime = parseFloat(getComputedStyle(this.#leftDisplay).getPropertyValue('transition').split(' ')[1].replace('s', '')) * 1000;
+
         // GETTING FIRST INFORMATIONS OF POKEDEX
         const { results } = await getAllPokemons();
         this.#pokemons = (results.map(item => ({ ...item, 'id': item.url.split('/')[6] }))).filter(item => item.id <= 10263);
-        this.getPokemonByIndex(this.#currentPokemonIndex);
+        this.getPokemonByIndex(this.#currentPokemonIndex, false);
 
-        console.log(this.#pokedexNode);
+        // console.log(this.#pokemons[this.#currentPokemonIndex]);
 
         this.#pokedexNode.addEventListener('click', this.power);
-        this.#btnLeft.addEventListener('click', this.previousPokemon);
-        this.#btnRight.addEventListener('click', this.nextPokemon);
+        this.#btnDirectionalLeft.addEventListener('click', this.previousPokemon);
+        this.#btnDirectionalRight.addEventListener('click', this.nextPokemon);
+        this.#btnDisplayDirUp.addEventListener('click', this.scrollUpRightDisplay);
+        this.#btnDisplayDirDown.addEventListener('click', this.scrollDownRightDisplay);
     }
 
     pokedexCoverIsClosed = () => !this.#pokedexCoverNode.classList.toString().includes('open-pokedex');
@@ -76,46 +103,61 @@ export class Pokedex {
     displayOff = display => !display.classList.toString().includes('powerOn');
 
     powerOn = () => {
-        const coverTransitionTime = parseFloat(getComputedStyle(this.#pokedexCoverNode).getPropertyValue('transition').split(' ')[1].replace('s', '')) * 1000;
-        const leftDisplayTransitionTime = parseFloat(getComputedStyle(this.#leftDisplay).getPropertyValue('transition').split(' ')[1].replace('s', '')) * 1000;
-
         // SETTING THE ANIMATION CLASS TO OPEN THE COVER
         this.#pokedexCoverNode.classList.add('open-pokedex');
 
         setTimeout(() => {
+            // TURN ON CRISTAL
+            this.#cristal.children[0].classList.add('glassOn');
+            this.#cristal.children[0].children.shine.classList.add('shining');
+
+            // TURN ON GREEN LED
+            this.#greenLed.classList.add('ledGrennOn');
+
             // DISPLAY LEFT TURN ON
             this.#leftDisplay.classList.add('powerOn');
 
             // WAIT THE TIME TRANSITION AND PLAY THE POKEMONS SOUND
             setTimeout(() => {
                 this.#pokemonSound.play();
-            }, leftDisplayTransitionTime);
-        }, coverTransitionTime);
+                this.setPokemonInfos();
+            }, this.#pokedexLeftDisplayTransitionTime);
+        }, this.#pokedexCoverTransitionTime);
     }
 
     powerOff = () => {
-        // SETTING THE ANIMATION CLASS TO OPEN THE COVER
-        this.#pokedexCoverNode.classList.remove('open-pokedex');
-
         // DISPLAY LEFT TURN OFF
         this.#leftDisplay.classList.remove('powerOn');
 
-        // RETURNS TO THE FIRST POKEMON WHEN THE DISPLAY IS TURNED OFF
-        // this.getPokemonByIndex(0);
+        setTimeout(() => {
+            // SETTING THE ANIMATION CLASS TO OPEN THE COVER
+            this.#pokedexCoverNode.classList.remove('open-pokedex');
+
+            // RETURNS TO THE FIRST POKEMON WHEN THE DISPLAY IS TURNED OFF
+            // this.getPokemonByIndex(0);
+
+            setTimeout(() => {
+                // TURN ON CRISTAL
+                this.#cristal.children[0].classList.remove('glassOn');
+                this.#cristal.children[0].children.shine.classList.remove('shining');
+
+                // TURN ON GREEN LED
+                this.#greenLed.classList.remove('ledGrennOn');
+
+                // SET THE RIGHT DISPLAY MESSAGE TO "LOADING..."
+                this.#rightDisplayInfos.innerHTML = '<div class="center">Loading...</div>';
+            }, this.#pokedexCoverTransitionTime);
+        }, this.#pokedexLeftDisplayTransitionTime);
     }
 
     power = event => {
         const isValidElement = ['cover', 'elements'].includes(event.target.classList[0]);
 
         if (isValidElement) {
-            if (this.pokedexCoverIsClosed()) {
+            if (this.pokedexCoverIsClosed())
                 this.powerOn();
-            } else {
-                // BLINK THE RED LED IF POKEDEX IS TURNED ON
-                this.blinkRedLed();
-
+            else
                 this.powerOff();
-            }
         }
     }
 
@@ -132,9 +174,54 @@ export class Pokedex {
         this.#currentHabitat = objHabitat;
     }
 
-    setCurrentPokemon = pokemon => {
+    setPokemonInfos = () => {
+        const { pokemon, specie } = this.#allCurrentPokemonInfos;
+
+        const infos = `
+            <table>
+                <thead>
+                    <tr>
+                        <th>#${(pokemon.id < 10) ? '00' + pokemon.id : (pokemon.id < 100) ? '0' + pokemon.id : pokemon.id}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Specie:</td>
+                        <td>${pokemon.name}</td>
+                    </tr>
+                    <tr>
+                        <td>Weight:</td>
+                        <td>${pokemon.infos.weight} Kg</td>
+                    </tr>
+                    <tr>
+                        <td>Habitat:</td>
+                        <td>${(specie.habitat !== null) ? specie.habitat.name : 'Unknow'}</td>
+                    </tr>
+                    <tr>
+                        <td>Type(s):</td>
+                        <td>${pokemon.types.map(item => item.type.name).join(', ')}</td>
+                    </tr>
+                    <tr>
+                        <td>Attack(s):</td>
+                    </tr>
+                </tbody>
+            </table>
+            <ul>${pokemon.moves.map(item => `<li>${item.move.name}</li>`).join('')}</ul>
+        `;
+
+        this.#rightDisplayInfos.innerHTML = infos;
+    }
+
+    setCurrentPokemon = updatePokemonInfos => {
+        const { pokemon } = this.#allCurrentPokemonInfos;
+
         this.#divPokemon.style.backgroundImage = `url(${pokemon.sprites.front_default})`;
         this.#pokemonSound.src = `https://play.pokemonshowdown.com/audio/cries/${pokemon.species.name}.mp3`;
+
+        if (updatePokemonInfos) {
+            this.setPokemonInfos(this.#allCurrentPokemonInfos);
+            this.resetScrollRightDisplay();
+        }
 
         if (!this.displayOff(this.#leftDisplay)) {
             setTimeout(() => {
@@ -149,22 +236,26 @@ export class Pokedex {
         // this.#leftDisplay.style.transition = `0s`;
     }
 
-    getPokemonByIndex = async index => {
+    getPokemonByIndex = async (index, updatePokemonInfos = true) => {
         const pokemon = new Pokemon(await getPokemonDetails(this.#pokemons[index].url));
         const specie = await getSpeciesDetails(pokemon.species.url);
 
         this.#currentPokemonIndex = index;
+        this.#allCurrentPokemonInfos = { pokemon, specie };
 
         if (specie.habitat !== null)
             this.changeHabitat((this.habitats.filter(objHabitat => objHabitat.name === specie.habitat.name))[0]);
         else
             this.changeHabitat(this.habitats[9]);
 
-        this.setCurrentPokemon(pokemon);
+        this.setCurrentPokemon(updatePokemonInfos);
     }
 
     previousPokemon = event => {
         event.stopPropagation();
+
+        // BLINK THE RED LED IF POKEDEX IS TURNED ON
+        this.blinkRedLed();
 
         if (!this.displayOff(this.#leftDisplay)) {
             let nextIndex = this.#currentPokemonIndex - 1;
@@ -179,11 +270,41 @@ export class Pokedex {
     nextPokemon = event => {
         event.stopPropagation();
 
+        // BLINK THE RED LED IF POKEDEX IS TURNED ON
+        this.blinkRedLed();
+
         if (!this.displayOff(this.#leftDisplay)) {
             if ((this.#currentPokemonIndex + 1) <= this.#pokemons.length - 1)
                 this.getPokemonByIndex(this.#currentPokemonIndex + 1);
             else
                 this.getPokemonByIndex(0);
         }
+    }
+
+    resetScrollRightDisplay = () => {
+        this.#scrollRightDisplay = 0;
+        this.#rightDisplayInfos.style.top = this.#scrollRightDisplay + 'px';
+    }
+
+    scrollUpRightDisplay = event => {
+        event.stopPropagation();
+
+        if (this.#scrollRightDisplay <= (this.#speedScrollRightDisplay * -1)) {
+            this.#scrollRightDisplay += this.#speedScrollRightDisplay;
+            this.#rightDisplayInfos.style.top = this.#scrollRightDisplay + 'px';
+        }
+    }
+
+    scrollDownRightDisplay = event => {
+        event.stopPropagation();
+
+        const endOfScroll = this.#rightDisplayInfos.scrollHeight;
+
+        if (this.#scrollRightDisplay >= ((endOfScroll - this.#speedScrollRightDisplay - 40)) * -1) {
+            this.#scrollRightDisplay -= this.#speedScrollRightDisplay;
+            this.#rightDisplayInfos.style.top = this.#scrollRightDisplay + 'px';
+        }
+
+        // console.log(`${this.#scrollRightDisplay} >= ${(endOfScroll - this.#speedScrollRightDisplay)}`);
     }
 }
--- js/main.js
+++ js/main.js
@@ -3,37 +3,41 @@ import { Pokedex } from "./classes/Pokedex.js";
 
 window.onload = () => {
     const redLed = document.querySelector('.led.red');
-    const redYellow = document.querySelector('.led.yellow');
-    const redGreen = document.querySelector('.led.green');
+    const yellowLed = document.querySelector('.led.yellow');
+    const greenLed = document.querySelector('.led.green');
     const cristal = document.querySelector('#cristal');
     const leftDisplay = document.querySelector('#leftDisplay');
     const btnPower = document.querySelector('#btnPower');
     const btnEnter = document.querySelector('#btnEnter');
-    const btnUp = document.querySelector('#btns .up');
-    const btnRight = document.querySelector('#btns .right');
-    const btnDown = document.querySelector('#btns .down');
-    const btnLeft = document.querySelector('#btns .left');
+    const btnDirectionalUp = document.querySelector('#btns .up');
+    const btnDirectionalRight = document.querySelector('#btns .right');
+    const btnDirectionalDown = document.querySelector('#btns .down');
+    const btnDirectionalLeft = document.querySelector('#btns .left');
     const rightDisplay = document.querySelector('#rightDisplay');
+    const btnDisplayDirUp = document.querySelector('#btnDisplayDirUp');
+    const btnDisplayDirDown = document.querySelector('#btnDisplayDirDown');
     const divPokemon = document.querySelector('#pokemon');
     const pokemonSound = document.querySelector('#pokemonSound');
     const pokedexNode = document.querySelector('#pokedex');
 
     const pokedex = new Pokedex({
         'redLed': redLed,
-        'redYellow': redYellow,
-        'redGreen': redGreen,
+        'yellowLed': yellowLed,
+        'greenLed': greenLed,
         'cristal': cristal,
         'leftDisplay': leftDisplay,
         'btnPower': btnPower,
         'btnEnter': btnEnter,
-        'btnUp': btnUp,
-        'btnRight': btnRight,
-        'btnDown': btnDown,
-        'btnLeft': btnLeft,
+        'btnDirectionalUp': btnDirectionalUp,
+        'btnDirectionalRight': btnDirectionalRight,
+        'btnDirectionalDown': btnDirectionalDown,
+        'btnDirectionalLeft': btnDirectionalLeft,
         'rightDisplay': rightDisplay,
         'divPokemon': divPokemon,
         'pokemonSound': pokemonSound,
-        'pokedexNode': pokedexNode
+        'pokedexNode': pokedexNode,
+        'btnDisplayDirUp': btnDisplayDirUp,
+        'btnDisplayDirDown': btnDisplayDirDown
     });
 
     pokedex.init();
